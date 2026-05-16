2d6ae1a7cdfce52b2c03c0350f9aa82fb9a756eb
Venca Srubar <vaclavcarlosrubar@gmail.cz>
2026-05-16 18:15:29 +0200

    Tested core logic

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ LIBS = -lmysqlcppconn
 
 all:
 	$(CXX) $(CXXFLAGS) src/*.cpp -o galaxy $(LIBS)
-	@echo "Hotovo! Spustíš pomocí: ./galaxy"
+	@echo "Done! Run the app with: ./galaxy"
 
 clean:
 	rm -f galaxy
-	@echo "Program smazán."
+	@echo "Program deleted."
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,15 @@
 #include <cppconn/exception.h>
 #include <cppconn/prepared_statement.h>
 #include "../include/Sith.h"
+#include "../include/Jedi.h"
 using namespace std;
 
 int main() {
     try {
         Sith* darth_vader = new Sith(1, "Darth Vader", "German", "Austrian German", 99);
+        Sith* kylo_ren = new Sith(2, "Kylo Ren", "Spanish", "Colombian / Mexican", 65);
         darth_vader->addStuds(3); 
+        kylo_ren->addStuds(4);
 
         sql::mysql::MySQL_Driver *driver;
         sql::Connection *con;
@@ -25,24 +28,24 @@ int main() {
         sql::PreparedStatement *pstmt;
         pstmt = con->prepareStatement("INSERT INTO characters (id, force_side, character_name, language, dialect, studs, anger_level) VALUES (?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE studs = ?, anger_level = ?");
         
-        pstmt->setInt(1, 1); 
+        pstmt->setInt(1, 2); 
         pstmt->setString(2, "Sith"); 
-        pstmt->setString(3, darth_vader->getCharacterName());
-        pstmt->setString(4, darth_vader->getLanguage());
-        pstmt->setString(5, darth_vader->getDialect());
-        pstmt->setInt(6, darth_vader->getStuds());
-        pstmt->setInt(7, darth_vader->getAngerLevel());
-        
-        pstmt->setInt(8, darth_vader->getStuds());
-        pstmt->setInt(9, darth_vader->getAngerLevel());
+        pstmt->setString(3, kylo_ren->getCharacterName());
+        pstmt->setString(4, kylo_ren->getLanguage());
+        pstmt->setString(5, kylo_ren->getDialect());
+        pstmt->setInt(6, kylo_ren->getStuds());
+        pstmt->setInt(7, kylo_ren->getAngerLevel());   
+        pstmt->setInt(8, kylo_ren->getStuds());
+        pstmt->setInt(9, kylo_ren->getAngerLevel());
 
         pstmt->executeUpdate();
         
-        cout << "May the force be with you! " << darth_vader->getCharacterName() << " was succesfully added into the database with " << darth_vader->getStuds() << " studs." << endl;
+        cout << "May the force be with you! " << kylo_ren->getCharacterName() << " was succesfully added into the database with " << kylo_ren->getStuds() << " studs." << endl;
 
         delete pstmt;
         delete con;
         delete darth_vader;
+        delete kylo_ren;
 
     } catch (sql::SQLException &e) {
         cout << "# ERR: SQL error!" << endl;
